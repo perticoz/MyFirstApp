42a1a2294b638147649140191a9ff95ef604cba7
Florent Perticoz <perticoz@hotmail.com>
2018-10-15 13:29:33 +0200

    quete 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    SignUpComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent implements OnInit {
+
+  bool = true;
+
+  User = {
+    name : 'Doe',
+    firstName : 'John',
+    age : 25,
+    quote : 'Java or not Java....',
+    photo : 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onAfficher() {
+    this.bool = !this.bool;
+  }
+}
