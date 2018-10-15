9fc461666d062c5bcc5d9d6121da2338f71c76b1
Florent Perticoz <perticoz@hotmail.com>
2018-10-15 15:10:20 +0200

    quete 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { MenuComponent } from './menu/menu.component';
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { AppComponent } from './app.component';
+
+const ROUTES: Routes =[
+    {path: 'user-profile', component: UserProfileComponent},
+    {path: 'sign-up', component: SignUpComponent},
+    { path: '', redirectTo: '/menu', pathMatch: 'full' },
+    { path: '**', redirectTo: '/user-profile', pathMatch: 'full' },
+];
+
+export {ROUTES};
